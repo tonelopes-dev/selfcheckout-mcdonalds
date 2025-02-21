50ecc6b17c886f7057632ccbcf3da2b80e0bfe3f
Tone Lopes <tonelopes.dev@gmail.com>
2025-02-21 11:35:21 -0300

    feat: add components and init pages projetcts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
@@ -18,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR">
+    <html lang="en">
       <body className={`${poppins.className} antialiased`}>{children}</body>
     </html>
   );
