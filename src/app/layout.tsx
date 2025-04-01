2abfa96a235f95545857845199e7ed7168e441dc
Tone Lopes <tonelopes.dev@gmail.com>
2025-04-01 17:25:19 -0300

    style: fix layout responsive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.className} antialiased`}>
+      <body className={`${poppins.className} mx-auto w-[50%] antialiased`}>
         <CartProvider>{children}</CartProvider>
         <Toaster />
       </body>
